49c708058a8ea36a740788d1a306c486cee750dd
Karan mavadiya <karanmavadiya38@gmail.com>
2025-07-30 14:37:34 +0530

    Error solve

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,13 +2,12 @@ import CreateNoteBookButton from "@/components/create-notebook-button";
 import NotebookCard from "@/components/notebookCard";
 import { PageWrapper } from "@/components/page-wrapper";
 import { getNotebooks } from "@/server/notebooks";
-
 export default async function Page() {
   const notebooks = await getNotebooks();
   return (
     <PageWrapper breadcrumbs={[{ label: "Dashboard", href: "/dashboard" }]}>
       <h1>NoteBooks</h1>
-      <CreateNoteBookButton />
+        <CreateNoteBookButton />
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {notebooks.success &&
           notebooks?.notebooks?.map((notebook) => (
